95640ade63d3ac7957d6eaf82b802a39ccfb0114
Alberto Riffaud <alriffaud@google.com>
2024-01-10 09:20:50 -0800

    Modification of 0-bubble_sort.c file.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,24 +10,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    int swap = 1, tmp;
-    size_t i;
+	int swap = 1, tmp;
+	size_t i;
 
-    if (array == NULL || size <= 1)
-	    return;
-    while (swap == 1)
-    {
-        swap = 0;
-        for (i = 1; i < size; i++)
-        {
-            if (array[i - 1] > array[i])
-            {
-                tmp = array[i - 1];
-                array[i - 1] = array[i];
-                array[i] = tmp;
-                swap = 1;
-                print_array(array, size);
-            }
-        }
-    }
+	while (swap == 1)
+	{
+		if (size == 0 || size == 1)
+			break;
+		swap = 0;
+		for (i = 1; i < size; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				tmp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = tmp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+	}
 }
